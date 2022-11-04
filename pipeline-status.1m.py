2bb9da0148fe935569cc9fb45fbcaf310e742c09
Sebastian <sebk@users.noreply.github.com>
2022-11-04 14:40:10 +0100

    show commit message

--- pipeline-status.1m.py
+++ pipeline-status.1m.py
@@ -6,7 +6,7 @@
 # <xbar.author>Sebastian Kruschwitz</xbar.author>
 # <xbar.author.github>sebk</xbar.author.github>
 # <xbar.desc>Monitor the status of your CodePipeline</xbar.desc>
-# <xbar.dependencies>python 3.10.x, boto3</xbar.dependencies>
+# <xbar.dependencies>python,boto3</xbar.dependencies>
 # <xbar.image>https://raw.githubusercontent.com/sebk/my-xbar-plugins/main/pipeline-status.png</xbar.image>
 # <xbar.abouturl>https://github.com/sebk/my-xbar-plugins/blob/main/README.md</xbar.abouturl>
 
@@ -14,7 +14,13 @@
 
 import os
 import json
+from enum import Enum
 import boto3
+
+class Status(Enum):
+    IN_PROGRESS = "InProgress"
+    FAILED = "Failed"
+    SUCCEEDED = "Succeeded"
 
 class PipelineStatus:
 
@@ -23,7 +29,7 @@ class PipelineStatus:
         self.execution_data_cache = {}
         self.cp_client = boto3.client("codepipeline")
         pipeline_result = self.cp_client.get_pipeline_state(name=self.pipeline_name)
-        self.pipeline_data = self.get_status(result=pipeline_result)
+        self.pipeline_data = self.get_data(result=pipeline_result)
 
     def request_execution(self, execution_id):
         if not execution_id:
@@ -39,7 +45,7 @@ class PipelineStatus:
             return message
         return ""
 
-    def get_status(self, result):
+    def get_data(self, result):
         stages = []
         for stage in result["stageStates"]:
             status = stage["latestExecution"]["status"] if "latestExecution" in stage else "unknown"
@@ -55,15 +61,20 @@ class PipelineStatus:
         
         return stages
 
-    def get_status_symbol(self, data) -> str:
-        any_progress = any(entry["status"] == "InProgress" for entry in data)
-        if any_progress: return ':hourglass_flowing_sand:'
+    def get_symbol(self, status) -> str:
+        if status == Status.IN_PROGRESS: return ':hourglass_flowing_sand:'
+        if status == Status.FAILED: return ':x:'
+        if status == Status.SUCCEEDED: return ':white_check_mark:'
 
-        any_error = any(entry["status"] == "Failed" for entry in data)
-        if any_error: return ':x:'
+    def get_stage_status_symbol(self, data) -> str:
+        any_progress = any(entry["status"] == Status.IN_PROGRESS.value for entry in data)
+        if any_progress: return self.get_symbol(Status.IN_PROGRESS)
 
-        all_succeeded = all(entry["status"] == "Succeeded" for entry in data)
-        if all_succeeded: return ':white_check_mark:'
+        any_error = any(entry["status"] == Status.FAILED.value for entry in data)
+        if any_error: return self.get_symbol(Status.FAILED)
+
+        all_succeeded = all(entry["status"] == Status.SUCCEEDED.value for entry in data)
+        if all_succeeded: return self.get_symbol(Status.SUCCEEDED)
 
         return ':warning:'
 
@@ -75,21 +86,22 @@ class PipelineStatus:
         #     case 'Failed': return ':x:'
         #     case _: return ':grey_question:'
         # Python 3.9.x:
-        if action["status"] == "Succeeded": return ':white_check_mark:'
-        if action["status"] == "InProgress": return ':hourglass_flowing_sand:'
-        if action["status"] == "Failed": return ':x:'
+        if action["status"] == Status.SUCCEEDED.value: return self.get_symbol(Status.SUCCEEDED)
+        if action["status"] == Status.IN_PROGRESS.value: return self.get_symbol(Status.IN_PROGRESS)
+        if action["status"] == Status.FAILED.value: return self.get_symbol(Status.FAILED)
         return ':grey_question:'
 
     def display_status(self):
-        emoji = self.get_status_symbol(data=self.pipeline_data)
+        emoji = self.get_stage_status_symbol(data=self.pipeline_data)
         status_string = f"{emoji} {self.pipeline_name}"
         print(f"{status_string}")
 
     def display_detailed_status(self):
         for stage in self.pipeline_data:
-            stage_emoji = self.get_status_symbol(stage["actions"])
+            stage_emoji = self.get_stage_status_symbol(stage["actions"])
             print(f"{stage_emoji} {stage['name']}")
-            print(f"-- {stage['message']}")
+            if stage["status"] != Status.SUCCEEDED.value:
+                print(f"-- {stage['message']}")
             for action in stage["actions"]:
                 action_emoji = self.get_action_status_symbol(action)
                 print(f"-- {action_emoji} {action['name']} ")
